cc0fe752cddefe30874545e3a6409cbf0066ee99
David Israel <davidisrael.rj@gmail.com>
2020-04-30 19:06:33 -0300

    Criando soma com duas variavel e exibindo

---
+++ Aula1.py
@@ -0,0 +1,10 @@
+print('Meu primeiro programa')
+# Somando dois valores
+
+a = 1 # a variavel 'a' recebe o valor 1
+
+b = 2 # a variavel 'b' recebe o valor 2
+
+soma = a + b # a variavel soma recebe a variavel a + b
+
+print(soma) # o resultado da soma sera exibida no console
